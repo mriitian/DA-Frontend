490cef1544d3da56089929fa92ff0d333bcac422
Vedant Parkhe <vedantparkhe2409@gmail.com>
2024-12-08 23:52:40 +0530

    first commit

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,184 +1,204 @@
 import { useFormik } from "formik";
 import axios from "axios";
-import { Grid, TextField, Button, Paper, Typography, Link, styled, IconButton, InputAdornment } from "@mui/material";
+import {
+  Grid,
+  TextField,
+  Button,
+  Paper,
+  Typography,
+  Link,
+  styled,
+  IconButton,
+  InputAdornment,
+} from "@mui/material";
 import * as yup from "yup";
 import { useDispatch, useSelector } from "react-redux";
 import loginSlice from "../store/slices/loginSlice";
-import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
-import VisibilityOffOutlinedIcon from '@mui/icons-material/VisibilityOffOutlined';
-import { useState } from 'react';
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Auth_API from "../utilities/api/authApis";
 
-const LoginContainer = styled('div')({
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-    backgroundColor: '#f2f8f9',
+const LoginContainer = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  height: "100vh",
+  backgroundColor: "#f2f8f9",
 });
 
 const LoginPaper = styled(Paper)({
-    padding: '20px',
-    borderRadius: '30px',
-    maxWidth: '400px',
-    width: '40%',
-    textAlign: 'center',
+  padding: "20px",
+  borderRadius: "30px",
+  maxWidth: "400px",
+  width: "40%",
+  textAlign: "center",
 });
 
-const Logo = styled('div')({
-    marginBottom: '20px',
+const Logo = styled("div")({
+  marginBottom: "20px",
 });
 
 const StyledTextField = styled(TextField)({
-    marginBottom: '10px',
+  marginBottom: "10px",
 });
 
 const StyledButton = styled(Button)({
-    marginTop: '10px',
-    backgroundColor: '#a1ede2',
-    color: '#000',
-    '&:hover': {
-        backgroundColor: '#8fdccf',
-    },
-    border: "1px solid #1b5e20",
-    borderRadius: "10px",
-    fontWeight: "bold"
+  marginTop: "10px",
+  backgroundColor: "#a1ede2",
+  color: "#000",
+  "&:hover": {
+    backgroundColor: "#8fdccf",
+  },
+  border: "1px solid #1b5e20",
+  borderRadius: "10px",
+  fontWeight: "bold",
 });
 
 const StyledLink = styled(Link)({
-    marginTop: '10px',
-    display: 'block',
-    color: '#00bfa6',
+  marginTop: "10px",
+  display: "block",
+  color: "#00bfa6",
 });
 
 const SignupLink = styled(Typography)({
-    marginTop: '20px',
-    color: '#a2acb6',
+  marginTop: "20px",
+  color: "#a2acb6",
 });
 
 const ErrorText = styled(Typography)({
-    marginTop: '10px',
-    color: 'red',
+  marginTop: "10px",
+  color: "red",
 });
 
 const Login = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const [showPassword, setShowPassword] = useState(false);
-    const [error, setError] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
 
-    const handleClickShowPassword = () => setShowPassword(!showPassword);
+  const handleClickShowPassword = () => setShowPassword(!showPassword);
 
-    const onSubmit = async (values, actions) => {
-        setError(''); 
-        try {
-            const response = await Auth_API.login(values);
-            console.log(response);
-            console.log("Logged in");
+  const onSubmit = async (values, actions) => {
+    setError("");
+    try {
+      const response = await Auth_API.login(values);
+      console.log(response);
+      console.log("Logged in");
 
-            dispatch(
-                loginSlice.actions.setAccountLog({
-                    user: response.data.user,
-                    token: response.data.access,
-                    refreshToken: response.data.refresh,
-                })
-            );
+      dispatch(
+        loginSlice.actions.setAccountLog({
+          user: response.data.user,
+          token: response.data.access,
+          refreshToken: response.data.refresh,
+        })
+      );
 
-            actions.resetForm();
-            navigate('/browse-data/open-source');
-        } catch (error) {
-            setError('Login failed. Please check your email and password and try again.');
-            console.log(error);
-        }
-    };
+      actions.resetForm();
+      navigate("/browse-data/open-source");
+    } catch (error) {
+      setError(
+        "Login failed. Please check your email and password and try again."
+      );
+      console.log(error);
+    }
+  };
 
-    const login_schema = yup.object().shape({
-        email: yup.string().email('Invalid email').required('Email is required'),
-        password: yup.string().required('Password is required'),
-    });
+  const login_schema = yup.object().shape({
+    email: yup.string().email("Invalid email").required("Email is required"),
+    password: yup.string().required("Password is required"),
+  });
 
-    const {
-        values,
-        handleBlur,
-        handleChange,
-        errors,
-        touched,
-        handleSubmit,
-        isSubmitting,
-    } = useFormik({
-        initialValues: {
-            email: '',
-            password: '',
-        },
-        validationSchema: login_schema,
-        onSubmit,
-    });
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    errors,
+    touched,
+    handleSubmit,
+    isSubmitting,
+  } = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: login_schema,
+    onSubmit,
+  });
 
-    return (
-        <LoginContainer>
-            <LoginPaper elevation={3}>
-                <Logo>
-                    <img src="../../public/login_logo.png" alt="Logo" width="40" />
-                </Logo>
-                <form noValidate onSubmit={handleSubmit}>
-                    <StyledTextField
-                        label="Email Address"
-                        variant="outlined"
-                        name="email"
-                        fullWidth
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={errors.email && touched.email}
-                        helperText={errors.email && touched.email && errors.email}
-                    />
-                    <StyledTextField
-                        label="Password"
-                        variant="outlined"
-                        name="password"
-                        type={showPassword ? 'text' : 'password'}
-                        fullWidth
-                        value={values.password}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={errors.password && touched.password}
-                        helperText={errors.password && touched.password && errors.password}
-                        InputProps={{
-                            endAdornment: (
-                                <InputAdornment position="end">
-                                    <IconButton
-                                        aria-label="toggle password visibility"
-                                        onClick={handleClickShowPassword}
-                                        edge="end"
-                                    >
-                                        {showPassword ? <VisibilityOffOutlinedIcon /> : <VisibilityOutlinedIcon />}
-                                    </IconButton>
-                                </InputAdornment>
-                            ),
-                        }}
-                    />
-                    
-                    <StyledButton type="submit" variant="contained" fullWidth disabled={isSubmitting}>
-                        Login
-                    </StyledButton>
-                    <StyledLink href="#">Forgot password?</StyledLink>
-                </form>
-                <SignupLink>
-                    Don't have an account yet? <Link href="/signup">Get started now</Link>
-                </SignupLink>
+  return (
+    <LoginContainer>
+      <LoginPaper elevation={3}>
+        <Logo>
+          <img src="../../public/login_logo.png" alt="Logo" width="40" />
+        </Logo>
+        <form noValidate onSubmit={handleSubmit}>
+          <StyledTextField
+            label="Email Address"
+            variant="outlined"
+            name="email"
+            fullWidth
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={errors.email && touched.email}
+            helperText={errors.email && touched.email && errors.email}
+          />
+          <StyledTextField
+            label="Password"
+            variant="outlined"
+            name="password"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={errors.password && touched.password}
+            helperText={errors.password && touched.password && errors.password}
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    edge="end"
+                  >
+                    {showPassword ? (
+                      <VisibilityOffOutlinedIcon />
+                    ) : (
+                      <VisibilityOutlinedIcon />
+                    )}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
+          />
 
-                {error && (
-                    <>
-                        {console.log(error)}
-                        <ErrorText>{error}</ErrorText>
-                    </> 
-                    
-                )}
-            </LoginPaper>
-        </LoginContainer>
-    );
+          <StyledButton
+            type="submit"
+            variant="contained"
+            fullWidth
+            disabled={isSubmitting}
+          >
+            Login
+          </StyledButton>
+          <StyledLink href="#">Forgot password?</StyledLink>
+        </form>
+        <SignupLink>
+          Don't have an account yet? <Link href="/signup">Get started now</Link>
+        </SignupLink>
+
+        {error && (
+          <>
+            {console.log(error)}
+            <ErrorText>{error}</ErrorText>
+          </>
+        )}
+      </LoginPaper>
+    </LoginContainer>
+  );
 };
 
 export default Login;
--- src/protected_route.jsx
+++ src/protected_route.jsx
@@ -3,17 +3,17 @@ import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
-const ProtectedRoute = ({ element}) => {
-    const auth = useSelector(state => state.login);
-    const token = auth.token;
+const ProtectedRoute = ({ element }) => {
+  const auth = useSelector((state) => state.login);
+  const token = auth.token;
 
-    if(token && auth.user){
-        return element;
-    }
-    else {
-        return <Navigate to="/login" />;
-    } 
-}
+  if (token && auth.user) {
+    return element;
+  } else {
+    return <Navigate to="/login" />;
+  }
 
- 
+  //   return element;
+};
+
 export default ProtectedRoute;
